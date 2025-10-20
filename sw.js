70705701b94410d4d5e5b5eda70a39512cdf2d78
Adrian <adrian.duran5248@gmail.com>
2025-10-20 12:48:37 -0600

    Simplify SW

--- sw.js
+++ sw.js
@@ -1,54 +1,14 @@
-const CACHE_NAME = "mi-cache-v2"; // Cambié a v2 para forzar actualización
-const urlsToCache = [
-    "./",
-    "./index.html",
-    "./manifest.json",
-    "./style.css"
-];
-
-// INSTALL
-self.addEventListener("install", event => {
-    console.log("SW: Instalando...");
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log("Cache abierto");
-                // Cachear archivos uno por uno para evitar errores
-                return Promise.all(
-                    urlsToCache.map(url => {
-                        return cache.add(url).catch(err => {
-                            console.log("Error cacheando:", url);
-                        });
-                    })
-                );
-            })
-    );
+// Service Worker mínimo - solo para que la PWA sea instalable
+self.addEventListener('install', () => {
+    console.log('SW instalado');
     self.skipWaiting();
 });
 
-// ACTIVATE
-self.addEventListener("activate", event => {
-    console.log("SW: Activando...");
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.filter(key => key !== CACHE_NAME)
-                .map(key => caches.delete(key))
-            )
-        )
-    );
+self.addEventListener('activate', () => {
+    console.log('SW activado');
     return self.clients.claim();
 });
 
-// FETCH
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-            .catch(() => {
-                return caches.match("./index.html");
-            })
-    );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
 });
